a8d9203d3aa7ecda9a8e053b2549052904069674
Fallen <102509054+Fallen786@users.noreply.github.com>
2022-07-02 20:01:55 +0530

    Updated TEST CODE.py
    made some changes to slim the code up as well as improving the user interface.
    -fallen

--- TEST CODE.py
+++ TEST CODE.py
@@ -3,7 +3,8 @@ import getpass
 questions = ['あ', 'い', 'う', 'え', 'お']
 def main():
     n=random.randint(0,4)
-    print (questions[n])
+    print("Q. What character is", questions[n])
+    answer = input("Ans: ")
     if n == 0:
         ans = 'a'
     if n == 1:
@@ -13,17 +14,16 @@ def main():
     if n == 3:
         ans = 'e'
     if n == 4:
-        ans = 'o'
-    answer = input('What character is this?\n')
+        ans = 'o'    
     if answer == ans:
         print("That is correct!")
         restart()
     else:
-        print('That is wrong...\n' + 'Correct answer was "'+ ans +'"')
+        print('That is wrong...\n' + f'Correct answer was "{ans}"')
         restart()
 def restart():
-    reply = getpass.getpass("Click Enter to continue. press F to exit\n")
-    if reply == 'f' or reply == 'F':
+    reply = getpass.getpass("Click Enter to continue. press F to exit\n").lower()
+    if reply == 'f':
         exit()
     else:
         main()
